ae980cbbdd7f731bf2c5e5e1cc71c357d904f1fb
Jarred Moyer <jarredds941@gmail.com>
2020-05-13 23:55:33 -0400

    Styled the radio information that represents the comments and score with a codepen style. margin/gap control needed

--- webapp/src/views/BarcodeRead.vue
+++ webapp/src/views/BarcodeRead.vue
@@ -7,31 +7,42 @@
                     <input type = "text" v-model = "form.submit_user">
                 </label>
 
-                <div class = "row">
-                comment
-                <label>
-                    Good turn
-                    <input type="radio" name="comment" value="good turn" v-model="form.comment"/>
-                </label>
-                <label>
-                    Completed activity
-                    <input type="radio" name="comment" value="completed activity" v-model="form.comment"/>
-                </label>
+                <div class="row">
+                <div class="col">
+                    <div class="col-xs-12">
+                        <br> Comment:
+                        <br>
+                        <div class="btn-group btn-group-vertical" data-toggle="buttons">
+                            <label class="btn active">
+                                <input type="radio" name='comment1' v-model = "form.comment" value="completed activity" ><i class="fa fa-circle-o fa-2x"></i><i class="fa fa-dot-circle-o fa-2x"></i> <span>  Completed Activity</span>
+                            </label>
+                            <label class="btn active">
+                                <input type="radio" name='comment2' v-model = "form.comment" value="good turn"><i class="fa fa-circle-o fa-2x"></i><i class="fa fa-dot-circle-o fa-2x"></i><span> Good turn daily</span>
+                            </label>
+
+                        </div>
+
+                    </div>
                 </div>
-                <div class = "row input-group">
-                    score
-                    <label>
-                        1
-                        <input type="radio" name="score" value="1" v-model="form.score"/>
-                    </label>
-                    <label>
-                        2
-                        <input type="radio" name="score" value="2" v-model="form.score"/>
-                    </label>
-                    <label>
-                        3
-                        <input type="radio" name="score" value="3" v-model="form.score"/>
-                    </label>
+
+                <div class="col">
+                    <div class="col-xs-12">
+                        <br> Score:
+                        <br>
+                        <div class="btn-group btn-group-vertical" data-toggle="buttons">
+                            <label class="btn active">
+                                <input type="radio" name='score1' v-model = "form.score" value="1" checked><i class="fa fa-circle-o fa-2x"></i><i class="fa fa-dot-circle-o fa-2x"></i> <span>  1</span>
+                            </label>
+                            <label class="btn active">
+                                <input type="radio" name='score2' v-model = "form.score" value="2" ><i class="fa fa-circle-o fa-2x"></i><i class="fa fa-dot-circle-o fa-2x"></i><span> 2</span>
+                            </label>
+                            <label class="btn active">
+                                <input type="radio" name='score3' v-model = "form.score" value="3" ><i class="fa fa-circle-o fa-2x"></i><i class="fa fa-dot-circle-o fa-2x"></i><span> 3</span>
+                            </label>
+                        </div>
+
+                    </div>
+                </div>
                 </div>
             </div>
 
@@ -130,5 +141,75 @@
 </script>
 
 <style scoped>
+    label.btn span {
+        font-size: 1.5em ;
+    }
 
+    label input[type="radio"] ~ i.fa.fa-circle-o{
+        color: #c8c8c8;    display: inline;
+    }
+    label input[type="radio"] ~ i.fa.fa-dot-circle-o{
+        display: none;
+    }
+    label input[type="radio"]:checked ~ i.fa.fa-circle-o{
+        display: none;
+    }
+    label input[type="radio"]:checked ~ i.fa.fa-dot-circle-o{
+        color: #7AA3CC;    display: inline;
+    }
+    label:hover input[type="radio"] ~ i.fa {
+        color: #7AA3CC;
+    }
+
+    label input[type="checkbox"] ~ i.fa.fa-square-o{
+        color: #c8c8c8;    display: inline;
+    }
+    label input[type="checkbox"] ~ i.fa.fa-check-square-o{
+        display: none;
+    }
+    label input[type="checkbox"]:checked ~ i.fa.fa-square-o{
+        display: none;
+    }
+    label input[type="checkbox"]:checked ~ i.fa.fa-check-square-o{
+        color: #7AA3CC;    display: inline;
+    }
+    label:hover input[type="checkbox"] ~ i.fa {
+        color: #7AA3CC;
+    }
+
+    div[data-toggle="buttons"] label.active{
+        color: #7AA3CC;
+    }
+
+    div[data-toggle="buttons"] label {
+        display: inline-block;
+        padding: 6px 12px;
+        margin-bottom: 0;
+        font-size: 14px;
+        font-weight: normal;
+        line-height: 2em;
+        text-align: left;
+        white-space: nowrap;
+        vertical-align: top;
+        cursor: pointer;
+        background-color: none;
+        border: 0px solid
+        #c8c8c8;
+        border-radius: 3px;
+        color: #c8c8c8;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        -o-user-select: none;
+        user-select: none;
+    }
+
+    div[data-toggle="buttons"] label:hover {
+        color: #7AA3CC;
+    }
+
+    div[data-toggle="buttons"] label:active, div[data-toggle="buttons"] label.active {
+        -webkit-box-shadow: none;
+        box-shadow: none;
+    }
 </style>
